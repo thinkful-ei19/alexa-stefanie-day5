2b9442368b38a057d45d28551e6524306543d625
Stefanie Carrasco <stefanieanncarrasco@gmail.com>
2018-02-23 11:50:43 -0500

    added first function pseudocode

--- rendering_shoppingList/index.js
+++ rendering_shoppingList/index.js
@@ -1,21 +1,44 @@
-
+const STORE = [
+  {name: 'apples', checked: false},
+  {name: 'oranges', checked: false},
+  {name: 'milk', checked: true},
+  {name: 'bread', checked: false}
+];
+  
 function renderShoppingList() {
-// A shopping list should be rendered to the page
-console.log('renderShoppingList works')
+//For each item in STORE, generate a string representing an <li> with:
+//  the item name rendered as inner text
+  //the item's index in the STORE set as a data attribute on the <li> (more on that in a moment)
+  //the item's checked state (true or false) rendered as the presence or absence of a CSS class for indicating checked items (specifically, .shopping-item__checked from index.css)
+  //Join together the individual item strings intoert the <li>s string inside the .js-shopping-list <ul> in one long string
+  // Ins the DOM.
+  console.log('renderShoppingList works');
+  const shoppingListItemsString = '<li>apples</li>';
+    // insert that HTML into the DOM
+    $('.js-shopping-list').html(shoppingListItemsString);
 }
 
 function handleNewItemSubmit() {
 // You should be able to add items to the list
-consoloe.log('handleNewItemSubmit works');
+  consoloe.log('handleNewItemSubmit works');
 }
 
 function handleItemCheckClicked() {
 // You should be able to check items on the list
-console.log('handleItemCheckClicked works');
+  console.log('handleItemCheckClicked works');
 }
 
 function handleDeleteItemClicked() {
 // You should be able to delete items from the list
-console.log('handleDeleteItemClicked works');
+  console.log('handleDeleteItemClicked works');
 }
+
+function handleShoppingList() {
+  renderShoppingList();
+  handleNewItemSubmit();
+  handleItemCheckClicked();
+  handleDeleteItemClicked();
+}
+
+$(handleShoppingList);
 
