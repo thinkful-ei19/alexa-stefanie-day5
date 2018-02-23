599c1d37938bcac7acc37b2e86bd9a1af589dc56
Alexa Scott <arscott4792@gmail.com>
2018-02-23 08:21:02 -0800

    added files

---
+++ rendering_shoppingList/index.js
@@ -0,0 +1,21 @@
+
+function renderShoppingList() {
+// A shopping list should be rendered to the page
+console.log('renderShoppingList works')
+}
+
+function handleNewItemSubmit() {
+// You should be able to add items to the list
+consoloe.log('handleNewItemSubmit works');
+}
+
+function handleItemCheckClicked() {
+// You should be able to check items on the list
+console.log('handleItemCheckClicked works');
+}
+
+function handleDeleteItemClicked() {
+// You should be able to delete items from the list
+console.log('handleDeleteItemClicked works');
+}
+
